8f3892f08a2906c4c0202ff7a12554582b148ce4
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-08-26 09:56:46 -0300

    feature: criando e estilizando os componentes defaultlayout e header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,21 @@
 import * as S from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+import Logo from '../../assets/logo.svg'
 
 export function Header() {
-  return <S.Container>Header</S.Container>
+  return (
+    <S.HeaderContainer>
+      <img src={Logo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </S.HeaderContainer>
+  )
 }
